a9c9f642166b5fe55450f8b94452cfc5b2ba228f
hentschellj <hentschellj@gmail.com>
2020-11-17 15:44:31 -0600

    Added Nav content

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import BC from '../assets/bc-logo.png';
+import MobileBC from '../assets/mobile-logo.png';
 
 class Nav extends React.Component {
   render() {
     return (
       <div className="nav">
-        <h1>Nav</h1>
+        <div className="container">
+          <div className="row">
+            <div className="nav-logo mobile"><img src={MobileBC} alt="B/C Logo" /></div>
+            <div className="nav-logo desktop"><img src={BC} alt="B/C Logo" /></div>
+            <h4 className="nav-content mobile">Style + Strategy + Sophistication</h4>
+            <h4 className="nav-content desktop">Blending Style + Strategy + Sophistication</h4>
+            <div className="nav-menu">Menu</div>
+          </div>
+        </div>
       </div>
     )
   }
