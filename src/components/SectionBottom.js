a9cfa9ea6ffcf68f68191886f813ac93bbf72173
hentschellj <hentschellj@gmail.com>
2020-11-17 16:36:29 -0600

    Added content for SectionBottom

--- src/components/SectionBottom.js
+++ src/components/SectionBottom.js
@@ -3,8 +3,19 @@ import React from 'react';
 class SectionBottom extends React.Component {
   render() {
     return (
-      <div className="sectionBottom">
-        <h1>Section Bottom</h1>
+      <div className="section-bottom">
+        <div className="container">
+          <div className="row">
+            <div className="bottom">
+              <h6 className="header-left-left">Lorem Ipsum</h6>
+              <h6 className="header-left-right">Lorem Ipsum</h6>
+            </div>
+            
+            <div className="bottom">
+              <h6>2015</h6>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
