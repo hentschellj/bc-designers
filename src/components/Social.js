585ae42a193c9a7fb9338260a707804d9124ded1
hentschellj <hentschellj@gmail.com>
2020-11-17 16:35:03 -0600

    Added content for Social

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import socialOne from '../assets/social1.png';
+import socialTwo from '../assets/social2.png';
+import socialThree from '../assets/social3.png';
+import socialFour from '../assets/social4.png';
 
 class Social extends React.Component {
   render() {
     return (
       <div className="social">
-        <h1>Social</h1>
+        <div className="container">
+          <div className="row">
+            <div className="social-top">
+              <h6 className="follow">Follow Us</h6>
+              <h6>Instagram <span>-</span> Pinterest <span>-</span> Twitter <span>-</span> Facebook</h6>
+            </div>
+
+            <div className="social-images">
+              <img src={socialOne} alt="Social One" />
+              <img src={socialTwo} alt="Social Two" />
+              <img src={socialThree} alt="Social Three" />
+              <img src={socialFour} alt="Social Four" />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
