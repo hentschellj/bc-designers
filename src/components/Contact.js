e773554a873b295e68950a42a6a2d3ccdd4bcad3
hentschellj <hentschellj@gmail.com>
2020-11-17 16:29:47 -0600

    Added content for Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,37 @@ class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        <h1>Contact</h1>
+        <div className="container">
+          <div className="row">
+            <div className="contact-top">
+              <h6>Get in touch with us!</h6>
+            </div>
+            
+            <div className="contact-locations">
+              <div className="location">
+                <h5>St. Louis</h5>
+                <hr />
+                <h6>reshma@bcdesigners.com</h6>
+              </div>
+              
+              <div className="location">
+                <h4>+</h4>
+              </div>
+              
+              <div className="location">
+                <h5>New York City</h5>
+                <hr />
+                <h6>lillian@bcdesigners.com</h6>
+              </div>
+            </div>
+            
+            <div className="contact-bottom">
+              <p className="top-bottom">Drop us a line!</p>
+              <p>123-123-1234</p>
+              <p>hello@bcdesigners.com</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
